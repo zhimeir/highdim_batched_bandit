f49f36addbcab6b303501dbc85565bc38f689ea0
zhimeir <zren@sh02-ln04.stanford.edu>
2021-05-30 11:15:51 -0700

    new results

--- code/submit_lasso.sh
+++ code/submit_lasso.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Parameters
 T=("6000")
-M_LIST=("3" "5" "6000")
+M_LIST=("3")
 
 
 # Slurm parameters
 PART=candes,hns,stat,pilanci      # Partition names
 MEMO=10G                         # Memory required (50GB)
-TIME=00-05:00:00                  # Time required (1d)
+TIME=01-00:00:00                  # Time required (1d)
 CORE=10                           # Cores required (10)
 
 # Assemble order prefix
@@ -20,10 +20,10 @@ mkdir -p $LOGS
 # Script to be run
 for M in "${M_LIST[@]}"; do 
   for SEED in {1..30}; do
-  SCRIPT="job.sh $T $M $SEED"
+  SCRIPT="lasso.sh $T $M $SEED"
   
   # Define job name for this chromosome
-  JOBN="sim-"$T"-"$M"-"$SEED
+  JOBN="lasso-"$T"-"$M"-"$SEED
   OUTF=$LOGS"/"$JOBN".out"
   ERRF=$LOGS"/"$JOBN".err"
 
